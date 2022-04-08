9c393542fbbecd6e009c4bcc580c8c4e52b49ba5
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-08 15:23:07 +0800

    Menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+
+namespace tpmodul7_1302204035;
+
+public class CovidConfig
+{
+    private String CONFIG1;
+    private int CONFIG2;
+    private String CONFIG3;
+    private String CONFIG4;
+    public CovidConfig()
+    {
+        //get path
+        string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        //get file
+        string file = File.ReadAllText(path+"\\CovidConfig.json");
+        //convert json
+        dynamic data = JsonConvert.DeserializeObject(file);
+        //set config value from json
+        CONFIG1 = data.satuan_suhu;
+        CONFIG2 = data.batas_hari_deman;
+        CONFIG3 = data.pesan_ditolak;
+        CONFIG4 = data.pesan_diterima;
+    }
+    
+    //getter
+    public String getCONFIG1()
+    {
+        return CONFIG1;
+    }
+
+    public String getSatuanSuhu()
+    {
+        return CONFIG1;
+    }
+    
+    public int getBatasHariDeman()
+    {
+        return CONFIG2;
+    }
+    
+    public String PesanPositif()
+    {
+        return CONFIG3;
+    }
+
+    public String PesanNegatif()
+    {
+        return CONFIG4;
+    }
+    
+    public void setSatuanSuhu(String suhu)
+    {
+        string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        string file = File.ReadAllText(path+"\\CovidConfig.json");
+        dynamic data = JsonConvert.DeserializeObject(file);
+        data.satuan_suhu = suhu;
+        string output = JsonConvert.SerializeObject(data, Formatting.Indented);
+        File.WriteAllText(path+"\\CovidConfig.json", output);
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -3,3 +3,36 @@
 using tpmodul7_1302204035;
 
 Console.WriteLine("Hello, World!");
+CovidConfig cfg = new CovidConfig();
+Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + cfg.getCONFIG1() + " : ");
+double suhu = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam ? : ");
+int hari = Convert.ToInt32(Console.ReadLine());
+//calculatedata
+    Boolean hasilSuhu = false;
+    Boolean hasilHari = false;
+    if (cfg.getSatuanSuhu() == "celcius") {
+        if (suhu >= 35.6 && suhu <= 37.5) {
+            hasilSuhu = true;
+        }
+    }
+    else if (cfg.getSatuanSuhu() == "fahrenheit") {
+        if (suhu >= 97.7 && suhu <= 99.5) {
+            hasilSuhu = true;
+        }
+    }
+
+    if (hari < cfg.getBatasHariDeman()) { 
+        hasilHari = true; 
+    }
+
+    if(hasilHari && hasilSuhu) {
+        Console.WriteLine(cfg.PesanPositif());
+    }
+    else
+    {
+        Console.WriteLine(cfg.PesanNegatif());
+    }
+//merubah satuan suhu
+    Console.WriteLine("Ubah satuan : ");
+    cfg.setSatuanSuhu(Console.ReadLine());
